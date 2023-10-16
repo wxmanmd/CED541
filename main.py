9452c2f449ae4ff1debe412a8c619261effe0fbc
wxmanmd <96307993+wxmanmd@users.noreply.github.com>
2023-10-16 19:51:26 -0400

    new pages

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+st.title("CED 541 Class Videos")
+
+st.write("This is where we will add the class videos. Use the menu on the left to choose the current module.")
---
+++ pages/module_1.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Module 1 Videos")
+st.header("Upload your video")
+
+uploaded_file = st.file_uploader("Choose a file")
