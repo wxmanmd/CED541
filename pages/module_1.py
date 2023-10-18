fb3aefda0e8f68ce01eddc946d2df2439227e29d
wxmanmd <96307993+wxmanmd@users.noreply.github.com>
2023-10-18 09:56:54 -0400

    updates

--- pages/module_1.py
+++ pages/module_1.py
@@ -4,3 +4,6 @@ st.title("Module 1 Videos")
 st.header("Upload your video")
 
 uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # add a progress bar
+    st.video(uploaded_file)
